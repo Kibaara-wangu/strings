25b389126c343b71281023bcd66f1de0993eb45a
kibaara-wangu <kibaarawanguggmail.com>
2024-03-08 17:21:45 -0500

    added files

---
+++ strings.js
@@ -0,0 +1,34 @@
+//1. Extract 4 characters from the string "extravaganza"
+let k = "extravaganza";
+console.log(k.slice(8));
+
+
+// 3. Count how many time "the" and "brown" is repeated
+let p= "The quick brown fox jumbed over the lazy dog";
+let m= "The";
+let b= "brown";
+console.log((m.match(/The/)).length);
+console.log((b.match(/brown/)).length);
+
+// 4. Using JavaScript, find the following words from the following strings:1. "are"const string1 = "The pupils are reading in the library";2. "sitting"const string2 = "The child was sitting on the table before it fell"
+let j= "The pupils are reading in the library";
+let l= "are";
+let z= "The child was sitting on the table before it fell";
+let y= "sitting"
+console.log(j.includes(l));
+console.log(z.includes(y));
+// 5. Convert the following strings into the specified format:1. UpperCase: "wonderful"2. LowerCase: "amazing", "UndERneath"3. Title case "A wonderful world"
+let mn= "wonderful";
+console.log(mn.toUpperCase());
+
+let jw= "amazing";
+console.log(jw.toLowerCase());
+
+// let gw= "Underneath";
+// console.log(gw.toTittleCase());
+
+
+
+
+
+
